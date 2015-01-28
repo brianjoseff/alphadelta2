bda5abddce088446011d5eeb96d914a92fc217d4
tibet000000 <Brian.D.P.Joseff@dartmouth.edu>
2015-01-28 12:20:52 -0800

    fixed key signing for AWS. images should work

--- app/models/adelphian.rb
+++ app/models/adelphian.rb
@@ -7,9 +7,9 @@ class Adelphian < ActiveRecord::Base
   validates_attachment_content_type :pdf, :content_type => ['application/pdf']
   #Rails.env.production? ? bucket: "readthis-prod" : bucket: "readthis-dev"
   def prod_s3_credentials
-     {:bucket => "alphadelta-pro", :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET'] }
+     {:bucket => "alphadelta-pro", :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET_KEY'] }
   end
   def s3_credentials
-      {:bucket => "alphadelta-dev", :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET']}
+      {:bucket => "alphadelta-dev", :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET_KEY']}
   end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,9 +1,19 @@
 class Photo < ActiveRecord::Base
   has_attached_file :image, :styles => { :medium => "400x300>", :thumb => "100x100>", original: "100%" },
     :storage => :s3, 
-    :s3_credentials => "#{Rails.root}/config/s3.yml", 
+    :s3_credentials => Proc.new{|a| Rails.env.production? ? a.instance.prod_s3_credentials :  a.instance.s3_credentials}, 
     :path => "/:attachment/:id/:style/:filename"
+  
+
+
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+  def prod_s3_credentials
+     {:bucket => "alphadelta-pro", :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET_KEY']}
+  end
+  def s3_credentials
+      {:bucket => "alphadelta-dev", :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET_KEY']}
+  end
 
   
 end
